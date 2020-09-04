ec161a66e208d33c9d538036e4710109bcf74250
Pinku <Pinku@DESKTOP-PQQ1F29.fibertel.com.ar>
2020-09-04 15:36:54 -0300

    Cambie el nombre del paquete

---
+++ src/main/java/tp2/Prueba.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2;
+
+/**
+ *
+ * @author Pinku
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        PingPong t1 = new PingPong("PING", 33);
+        PingPong t2 = new PingPong("PONG", 10);
+        // Activación
+        t1.start();
+        t2.start();
+        // Espera unos segundos
+        try {
+            System.out.println("duerme");
+            Thread.sleep(5000);
+            System.out.println("duerme");
+        } catch (InterruptedException e) {
+        };
+        // Finaliza la ejecución de los threads
+    }
+}
