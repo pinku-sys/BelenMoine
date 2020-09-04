94e8feb330cc218c941682b4689386eb19dd2832
Pinku <Pinku@DESKTOP-PQQ1F29.fibertel.com.ar>
2020-09-04 16:27:54 -0300

    Agrego el metodo esperar x segundos y procesar compra

--- src/main/java/tp2/Cajera.java
+++ src/main/java/tp2/Cajera.java
@@ -10,7 +10,31 @@ package tp2;
  * @author Pinku
  */
 public class Cajera {
+
+    private String nombre;
+
+    public Cajera(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void procesarCompra(ClienteCajera cliente, long timeStamp) throws InterruptedException {
+        System.out.println("La cajera " + this.nombre + "COMIENZA A PROCESAR LA COMPRA DEL CLIENTE "
+                + cliente.getNombre() + "EN EL TIEMPO: " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+
+        for (int i = 0; i < cliente.getCarroCompra().length; i++) {
+            this.esperarXsegundos(cliente.getCarroCompra()[i]);
+            System.out.println("Procesado el producto " + (i + 1)
+                    + "->Tiempo: " + (System.currentTimeMillis() - timeStamp)
+                    / 1000 + "seg");
+        }
+        System.out.println("La cajera " + this.nombre +" HA TERMINADO DE PROCESAR " + cliente.getNombre() +  "EN EL TIEMPO: "+
+(System.currentTimeMillis() - timeStamp)/ 1000 + "seg");
+    }
     
+    public void esperarXsegundos(int obj) throws InterruptedException {
+        Thread.sleep(obj);
+    }
+
     
     
 }
