670945710fc57c5c93280d4c5b1f821074338017
Pinku <Pinku@DESKTOP-PQQ1F29.fibertel.com.ar>
2020-09-04 16:06:06 -0300

    Agregue el codigo al metodo run

--- src/main/java/tp2/ClienteRunnable.java
+++ src/main/java/tp2/ClienteRunnable.java
@@ -12,7 +12,16 @@ package tp2;
 public class ClienteRunnable implements Runnable{
     
     public void run(){
+        System.out.println("soy"+ Thread.currentThread().getName());
+        Recurso.uso();
+        try {
+            Thread.sleep(2000);
+            
+        } catch (InterruptedException e) {
+            
+        }
+    }
         
     }
     
-}
+
