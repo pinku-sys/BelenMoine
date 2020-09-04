7d2a4223a10f135f9cacb94ce55caae8a31ecc09
Pinku <Pinku@DESKTOP-PQQ1F29.fibertel.com.ar>
2020-09-04 15:33:09 -0300

    Cree el repositorio

---
+++ src/main/java/tp2/PingPong.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teoriaPrueba;
+
+/**
+ *
+ * @author Pinku
+ */
+public class PingPong extends Thread {
+
+    private String cadena; // Lo que va a escribir.
+    private int delay; // Tiempo entre escritura
+
+    public PingPong(String cartel, int cantMs) {
+        cadena = cartel;
+        delay = cantMs;
+    }
+
+    ;
+ public void run() {
+        for (int i = 1; i < delay * 10; i++) {
+            System.out.print(cadena + "");
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+
+            }
+        }
+    } //fin método run() 
+}
