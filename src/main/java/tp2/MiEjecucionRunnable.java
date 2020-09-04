acb7f59537f2ca15544e125a20ad712e9121c5ff
Pinku <Pinku@DESKTOP-PQQ1F29.fibertel.com.ar>
2020-09-04 16:04:46 -0300

    Cree la clase MiEjecucionRunnable

---
+++ src/main/java/tp2/MiEjecucionRunnable.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2;
+
+/**
+ *
+ * @author Pinku
+ */
+public class MiEjecucionRunnable implements Runnable{
+    
+    public void run(){
+        
+    }
+    
+}
